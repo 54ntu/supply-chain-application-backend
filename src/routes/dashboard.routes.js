46b0082fc168de4ed9a39582b4ef001b7c0c1c64
54ntu <chaudharysantaram8@gmail.com>
2025-02-12 15:15:26 +0545

    dashboard routes are updated with appropriate role based permissions

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const { DashboardController } = require("../controller/dashboard.controller");
+const { UserMiddleware } = require("../middleware/auth.middleware");
 const dashboardRouter = express.Router();
 
-dashboardRouter.route("/stock").get(DashboardController.getStockSummary);
+dashboardRouter
+  .route("/stock")
+  .get(UserMiddleware.isUserLoggedIn, DashboardController.getStockSummary);
 
 dashboardRouter
   .route("/orders")
-  .get(DashboardController.getOrderShipentSummary);
+  .get(
+    UserMiddleware.isUserLoggedIn,
+    UserMiddleware.isDistributor,
+    DashboardController.getOrderShipentSummary
+  );
 dashboardRouter
   .route("/salessummary")
-  .get(DashboardController.getTotalSalesSummary);
+  .get(UserMiddleware.isUserLoggedIn, DashboardController.getTotalSalesSummary);
 
 module.exports = {
   dashboardRouter,
